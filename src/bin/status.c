f0d1cf8d92a018c6603d5e425fed50624cb90740
Patrick CHEVALIER <patrick.chevalier@openwide.fr>
2012-03-27 17:14:45 +0200

    done the gengrid system. we can watch all page of project in the gengrid (status bar) and see a preview of them.

--- src/bin/status.c
+++ src/bin/status.c
@@ -37,11 +37,11 @@ refresh_childs(Elegance_Content *data)
   Evas_Object *new, *lay;
 
   printf("        adding %s\n", data->name);
-  printf("%s %p %p\n", data->name, data, data->obj);
   lay = elm_layout_add(design_win);
-  evas_object_size_hint_weight_set(lay, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-  elm_layout_theme_set(lay, "layout", "application", "add_in_object");
 
+  elm_layout_theme_set(lay, "layout", "application", "add_in_object");
+  evas_object_size_hint_weight_set(lay, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+  evas_object_size_hint_align_set(lay, EVAS_HINT_FILL, EVAS_HINT_FILL);
   if(!strcmp(data->tool.type, "evas"))
     new = data->tool.function_add(evas_object_evas_get(design_win));
   else
@@ -56,14 +56,18 @@ refresh_childs(Elegance_Content *data)
   {
     Eina_List *l_subchild;
     Elegance_Content *data_subchild;
+    Evas_Object *lay_sub;
 
     EINA_LIST_FOREACH(data->child, l_subchild, data_subchild)
-      data->tool.function_pack(new,
-			       refresh_childs(data_subchild),
+    {
+      printf("        packer: %s\n", data->tool.name);
+      lay_sub = refresh_childs(data_subchild);
+      data->tool.function_pack(new, lay_sub,
 			       NULL, NULL, NULL, NULL);
+    }
   }
-
-  printf("\n\n");
+  evas_object_show(lay);
+  printf("refresh %s finish\n", data->name);
   return lay;
 }
 
@@ -79,12 +83,10 @@ refresh_layout(Elegance_Gengrid_Item *item)
 
     printf("    adding %s\n", data->name);
 
-    printf("%s %p %p\n", data->name, data, data->obj);
     if(!strcmp(data->tool.type, "evas"))
       new = data->tool.function_add(evas_object_evas_get(design_win));
     else
       new = data->tool.function_add(design_win);
-    printf("******* new: %p item->lay: %p\n", new, item->lay);
     elm_object_part_content_set(item->lay,
 				"elm.swallow.add_in_object", new);
     edje_object_color_class_set(item->lay, "elegance_1",
@@ -105,27 +107,15 @@ refresh_layout(Elegance_Gengrid_Item *item)
 
       EINA_LIST_FOREACH(data->child, l_subchild, data_subchild)
       {
+      	printf("    data: %s\n", data->tool.name);
 	lay = refresh_childs(data_subchild);
-	printf("lay: %p - new: %p - item: %p\n", lay, new, item);
-	data->tool.function_pack(new, lay,
-				 NULL, NULL, NULL, NULL);
+      	data->tool.function_pack(new, lay,
+      				 NULL, NULL, NULL, NULL);
       }
     }
   }
-  printf("\n\n");
-}
-
-static void
-refresh_layouts(void)
-{
-
-  Eina_List *l;
-  Elegance_Gengrid_Item *data;
-
-  EINA_LIST_FOREACH(status_grid->items, l, data)
-  {
-    refresh_layout(data);
-  }
+  printf("refresh finish\n");
+  return item->lay;
 }
 
 static Evas_Object *
@@ -137,9 +127,18 @@ _grid_content_get(void        *data,
 
   if (!strcmp(part, "elm.swallow.icon"))
   {
-    printf("part: %p - obj: %p\n", part, obj);
-    refresh_layouts();
-    return item->lay;
+    Evas_Object *lay;
+
+    lay = elm_layout_add(design_win);
+    evas_object_size_hint_weight_set(lay, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    evas_object_size_hint_align_set(lay, EVAS_HINT_FILL, EVAS_HINT_FILL);
+    elm_layout_theme_set(lay, "layout", "application", "add_in_object");
+    evas_object_show(lay);
+
+    item->lay = lay;
+
+    lay = refresh_layout(item);
+    return lay;
   }
   return NULL;
 }
@@ -154,19 +153,11 @@ status_refresh(void)
 
   if(status_grid->nb_items == 0)
   {
-    Evas_Object *lay;
-
     printf("append\n");
-
-    lay = elm_layout_add(design_win);
-    elm_layout_theme_set(lay, "layout", "application", "add_in_object");
-    evas_object_show(lay);
-
-    printf("item->lay: %p\n", lay);
 
     item = malloc(sizeof(Elegance_Gengrid_Item));
     item->page = actual_page;
-    item->lay = lay;
+    item->lay = NULL;
     item->name = actual_page->name;
     status_grid->items = eina_list_append(status_grid->items,
 					  item);
@@ -208,8 +199,6 @@ status_add(Evas_Object *win)
   status_grid->gic->func.del = _grid_del;
 
   evas_object_show(grid);
-  printf("gengrid: %p - status_grid: %p - gic: %p - win: %p\n",
-	 grid, status_grid, status_grid->gic, win);
 
   return grid;
 }
